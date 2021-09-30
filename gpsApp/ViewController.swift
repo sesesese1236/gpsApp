f1c3fc5d93fd3a9c8a33c7f18ec15d7722b8c6bf
先生 <ateacher@iMac202003noiMac.local>
2021-09-30 16:05:01 +0900

    initialize step 2

--- gpsApp/ViewController.swift
+++ gpsApp/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    @IBOutlet weak var labelLat: UILabel!
+    @IBOutlet weak var labelLon: UILabel!
+    
+    var locationManager : CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager=CLLocationManager()
+        
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if status == CLAuthorizationStatus.authorizedAlways{
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
     }
 
 
+    @IBAction func clickGet(_ sender: UIButton) {
+    }
+    func locationManager(manager:CLLocationManager,didUpdateLocations locations:[CLLocation]){
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longtitude = location?.coordinate.longitude
+        print("lantidude\(latitude!). longtitude\(longtitude!)")
+    }
 }
 
