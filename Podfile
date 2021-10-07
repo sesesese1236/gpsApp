e187cc9d39d28bcad75b3d0459279cac2212e70d
WENDRA RIANTO <2020jn23@iMac202003noiMac.local>
2021-10-07 16:19:26 +0900

    insert map view to the application

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'gpsApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for gpsApp
+
+  target 'gpsAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'gpsAppUITests' do
+    # Pods for testing
+  end
+
+  pod 'RealmSwift'
+
+end
--- gpsApp/ViewController.swift
+++ gpsApp/ViewController.swift
@@ -7,35 +7,106 @@
 
 import UIKit
 import CoreLocation
+import MapKit
+import RealmSwift
 
+class YourExistence : Object{
+    @objc dynamic var id:String? = date+time
+    @objc dynamic var time:String? = nil
+    @objc dynamic var date:String? = nil
+    @objc dynamic var latitude:String? = nil
+    @objc dynamic var longtitude:String? = nil
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
 class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    
     @IBOutlet weak var labelLat: UILabel!
     @IBOutlet weak var labelLon: UILabel!
-    
+    @IBOutlet weak var btnStop: UIButton!
+    @IBOutlet weak var btnStart: UIButton!
+    let status = CLLocationManager.authorizationStatus()
     var locationManager : CLLocationManager!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         locationManager=CLLocationManager()
         
         locationManager.requestWhenInUseAuthorization()
-        let status = CLLocationManager.authorizationStatus()
-        if status == CLAuthorizationStatus.authorizedAlways{
+        if checkAuthorization(){
             locationManager.delegate = self
             locationManager.distanceFilter = 10
+//            locationManager.startUpdatingLocation()
+        }
+        btnStop.isEnabled = false
+        
+        mapView.mapType = MKMapType.hybrid
+        mapUpdate()
+    }
+
+
+    @IBAction func clickStart(_ sender: UIButton) {
+        if checkAuthorization(){
             locationManager.startUpdatingLocation()
         }
+        mapUpdate()
+        switchOnOff()
     }
-
-
+    
     @IBAction func clickGet(_ sender: UIButton) {
+        if checkAuthorization(){
+            locationManager.startUpdatingLocation()
+        }
+        mapUpdate()
+        sleep(1)
+        locationManager.stopUpdatingLocation()
+        mapView.userTrackingMode = MKUserTrackingMode.none
     }
-    func locationManager(manager:CLLocationManager,didUpdateLocations locations:[CLLocation]){
+    
+    @IBAction func clickStop(_ sender: UIButton) {
+        if checkAuthorization(){
+            locationManager.stopUpdatingLocation()
+        }
+        mapView.userTrackingMode = MKUserTrackingMode.none
+        switchOnOff()
+    }
+    
+    
+    func mapUpdate(){
+        mapView.setCenter(mapView.userLocation.coordinate, animated: true)
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+    }
+    
+    
+    func checkAuthorization() -> Bool{
+        if status == CLAuthorizationStatus.authorizedAlways ||
+            status == CLAuthorizationStatus.authorizedWhenInUse {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
+    func switchOnOff(){
+        btnStart.isEnabled = !(btnStart.isEnabled)
+        btnStop.isEnabled = !(btnStop.isEnabled)
+    }
+    
+    
+    func locationManager(_ manager:CLLocationManager,didUpdateLocations locations:[CLLocation]){
         let location = locations.first
         let latitude = location?.coordinate.latitude
         let longtitude = location?.coordinate.longitude
         print("lantidude\(latitude!). longtitude\(longtitude!)")
+        labelLat.text = String(latitude!)
+        labelLon.text = String(longtitude!)
     }
 }
 
